f130c0ba4b0de3ecd627a2b148f3ef9009f328e2
51967 <51967@192.168.1.5>
2021-04-25 11:01:59 -0500

    Actualizacion 25 / 4

--- src/com/senati/eti/Caso8.java
+++ src/com/senati/eti/Caso8.java
@@ -7,7 +7,7 @@ public class Caso8 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Ingrese lado del cuadrado: ");
+		System.out.print("Ingrese lado del cuadrado: ");
 		int lado = sc.nextInt();
 		
 		int area = (int)Math.pow(lado, 2);
